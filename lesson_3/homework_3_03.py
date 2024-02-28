3411295bd309cfa013bb8d5fba46aaef352525a2
Dimm <slim.slk@gmail.com>
2024-02-28 16:55:58 +0100

    refactored code

--- lesson_3/homework_3_03.py
+++ lesson_3/homework_3_03.py
@@ -1,8 +1,11 @@
-first_coordinates = input('Введите координаты первой точки (x1, y1):').split(', ')
-second_coordinates = input('Введите координаты второй точки (x2, y2):').split(', ')
+first_coordinates = input('Введите координаты первой точки (x1, y1): ').split(', ')
+second_coordinates = input('Введите координаты второй точки (x2, y2): ').split(', ')
 
+x1 = float(first_coordinates[0])
+y1 = float(first_coordinates[1])
+x2 = float(second_coordinates[0])
+y2 = float(second_coordinates[1])
 
-distance = ((float(second_coordinates[0]) - float(first_coordinates[0])) ** 2
-            + (float(second_coordinates[1]) - float(first_coordinates[1])) ** 2) ** 0.5
+distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 print('Расстояние между точками:', distance)
