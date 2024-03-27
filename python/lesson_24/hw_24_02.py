b2a51e2286afec11290a35dd5bc80e70cb334aeb
Dimm <slim.slk@gmail.com>
2024-03-27 21:12:17 +0100

    added python hw 23-24

---
+++ python/lesson_24/hw_24_02.py
@@ -0,0 +1,13 @@
+# Напишите генератор, который будет генерировать арифметическую прогрессию
+
+def arithmetic_progression(start_number, step):
+    while True:
+        yield start_number
+        start_number += step
+
+
+nums = list(map(int, input("Enter the start number and progression step separated by a space: ").split()))
+ap = arithmetic_progression(nums[0], nums[1])
+count = int(input("Enter the number of sequence elements to display: "))
+for _ in range(count):
+    print(next(ap))
