4fd3497741bb049740d509e0089e2452b8536681
Dimm <slim.slk@gmail.com>
2024-04-05 11:51:38 +0200

    fixed sql hw 12

--- sql/lesson_12/hw_12_01.sql
+++ sql/lesson_12/hw_12_01.sql
@@ -1,21 +1,23 @@
 USE world;
 
--- 1. Используя ранжирующие функции, вывести количество городов  в каждой стране для каждой страны.
+-- 1. Вывести количество городов для каждой страны.
 -- Результат должен содержать CountryCode, CityCount (количество городов в стране).
 SELECT CountryCode,
-    COUNT(*) OVER (PARTITION BY CountryCode) AS CityCount
-FROM city;
+    COUNT(*) AS CityCount
+FROM city
+GROUP BY CountryCode;
 
 -- Поменяйте запрос с использованием
 -- джойнов так, чтобы выводилось название страны вместо CountryCode.
 SELECT country.Name,
-    COUNT(*) OVER (PARTITION BY CountryCode) AS CityCount
+    COUNT(*) AS CityCount
 FROM city
 INNER JOIN country
-    ON city.CountryCode = country.Code;
+    ON city.CountryCode = country.Code
+GROUP BY CountryCode;
 
 
--- 2. Используя ранжирующие функции, Вывести список стран с продолжительностью жизнью и средней продолжительностью жизни.
+-- 2. Используя оконные функции, вывести список стран с продолжительностью жизнью и средней продолжительностью жизни.
 SELECT
     Name,
     LifeExpectancy,
@@ -31,9 +33,9 @@ FROM country;
 
 -- 4. Используя ранжирующие функции, вывести третью страну с самой высокой продолжительностью жизни.
 SELECT *
-FROM    (SELECT
+FROM (SELECT
             Name,
             LifeExpectancy,
             ROW_NUMBER() OVER (ORDER BY LifeExpectancy DESC) AS life_exp_rank
-        FROM country) tmp
+     FROM country) tmp
 WHERE life_exp_rank = 3;
